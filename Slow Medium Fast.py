b9ca86b392aa92e9c17282ae6760de5ef40e1ec7
kinshuk-code-1729 <kinshuk2002.king@gmail.com>
2024-02-17 20:11:34 +0530

    line styles added

--- Slow Medium Fast.py
+++ Slow Medium Fast.py
@@ -1,8 +1,9 @@
-import matplotlib.pyplot as plt 
-import numpy as np 
-x = np.arange(1, 5) 
-plt.plot(x, x*1.5, label='Normal') 
-plt.plot(x, x*3.0, label='Fast') 
-plt.plot(x, x/3.0, label='Slow') 
-plt.legend() 
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(1, 5)
+plt.plot(x, x * 1.5, label='Normal', linewidth=3, linestyle='dashed')
+plt.plot(x, x * 3.0, label='Fast', linewidth=3, linestyle='dashdot')
+plt.plot(x, x / 3.0, label='Slow', linewidth=3, linestyle='dotted')
+plt.legend()
 plt.show()
